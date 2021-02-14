df6dc62495c5d5da58841737456fa57631f360d1
jussisahlstrom <70265050+jussisahlstrom@users.noreply.github.com>
2021-02-14 20:42:46 +0200

    commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const carRouter = require('./routes/car');
+
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.set('views', path.join(dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/car', carRouter);
+app.use('/user', usersRouter);
+
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+app.use(function(err, req, res, next) {
+  
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
