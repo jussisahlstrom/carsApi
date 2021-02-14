c6f9d8fe25bee81a010f262eb6b903ec36a4c72e
jussisahlstrom <70265050+jussisahlstrom@users.noreply.github.com>
2021-02-14 21:25:38 +0200

    Update car_model.js

---
+++ models/car_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const book = {
+  get: function(callback) {
+    return db.query('select * from book order by idcar desc', callback);
+  },
+  getById: function(id, callback) {
+    return db.query('select * from book where idcar=?', [id], callback);
+  },
+  add: function(car, callback) {
+    return db.query(
+      'insert into car (brand,model,yearmodel) values(?,?,?)',
+      [car.brand, car.model, car.yearmodel],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from car where idcar=?', [id], callback);
+  },
+  update: function(brand, model, yearmodel) {
+    return db.query(
+      'update book set name=?,author=?, isbn=? where idcar=?',
+      [car.brand, car.model, car.yearmodel id],
+      callback
+    );
+  }
+};
+module.exports = car;
