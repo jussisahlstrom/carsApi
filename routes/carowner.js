e64171f4a6e329600c6da365bf5a6da118ed9158
jussisahlstrom <70265050+jussisahlstrom@users.noreply.github.com>
2021-02-21 23:21:52 +0200

    Update carowner.js

--- routes/carowner.js
+++ routes/carowner.js
@@ -1,29 +1,76 @@
-const express = require('express');
+const db = require('../database');
+
+const carowner = {
+  get: function(callback) {
+    return db.query('SELECT * FROM carowner ORDER BY idcarowner DESC', callback);
+  },
+  getById: function(id, callback) {
+    return db.query('select * from carowner where idcarowner=?', [id], callback);
+  },
+  carowner_model: function(callback) {
+    return db.query(select owner.idowner, owner.firstname, owner.lastname,
+    group_concat(concat(car.brand, ' ', car.model) separator', ') as "Cars of the person"
+    from owner
+    join carowner on carowner.idowner=owner.idowner
+    join car ON car.idcar=carowner.idcar
+    group by owner.idowner;, callback);
+  },
+  add: function(carowner, callback) {
+    return db.query(
+      'insert into carowner (idcar,idowner) values(?,?)',
+      [carowner.idcar, carowner.idowner],
+      callback
+    );
+  },
+  delete: function(idcarowner, callback) {
+    return db.query('delete from car where idcarowner=?', [id], callback);
+  },
+  update: function(idcarowner, carowner, callback) {
+    return db.query(
+      'update carowner set idcar=?,idowner=? where idcarowner=?',
+      [carowner.idcar, carowner.idowner, idcarowner],
+      callback
+    );
+  }
+};
+module.exports = carowner;
+[23.19] Ayrton: const express = require('express');
 const router = express.Router();
 const carowner = require('../models/carowner_model');
 
-router.get('/:id?',
- function(request, response) {
-  if (request.params.id) {
-    carowner.getById(request.params.id, function(err, dbResult) {
+router.get('/', function(request, response) {
+  carowner.get(function(err, dbResult) {
       if (err) {
-        response.json(err);
+          response.json(err);
       } else {
-        console.log(dbResult[0]);
-        response.json(dbResult[0]);
+          response.json(dbResult);
       }
-    });
-  } else {
-    carowner.get(function(err, dbResult) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(dbResult);
-      }
-    });
-  }
+  });
 });
 
+
+
+router.get('/cars', function(request, response) {
+  carowner.OwnerOfCars( function(err, dbResult) {
+      if (err) {
+           response.json(err) ;
+      } else {
+           response.json(dbResult) ;
+      } ;
+  }); 
+});
+
+router.get('/:id?', function(request, response,) {
+  if (request.params.id) {
+      carowner.getById(request.params.id, function(err, dbResult) {
+          if (err) {
+          response.json(err);
+          } else {
+          response.json(dbResult);
+          }
+      });
+  }
+});
 
 router.post('/', 
 function(request, response) {
